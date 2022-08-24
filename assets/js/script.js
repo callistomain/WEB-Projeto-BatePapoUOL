3e031d45bbfbe5ae9b0c9c50a183c4759c65282d
Alysson Marley Dias <callistomain@gmail.com>
2022-08-24 10:55:25 -0300

    Become fully functional, refactor to clean up nedeed

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,14 @@ const loginInput = loginForm.elements["name"];
 const loginButton = loginForm.querySelector("button");
 const loginSubmit = loginForm.querySelector(".submit");
 const loginLoading = loginForm.querySelector(".loading");
-const urlLogin = "https://mock-api.driven.com.br/api/v6/uol/participants";
+const urlParticipants = "https://mock-api.driven.com.br/api/v6/uol/participants";
 const urlStatus = "https://mock-api.driven.com.br/api/v6/uol/status";
+const urlMessages = "https://mock-api.driven.com.br/api/v6/uol/messages";
 
 loginForm.addEventListener("submit", validateLogin);
 loginInput.addEventListener("input", updateButton);
+updateParticipants();
+updateMessages();
 
 function validateLogin(e) {
     e.preventDefault();
@@ -24,20 +27,25 @@ function validateLogin(e) {
     };
     const data = createPostData(obj);
     
-    fetch(urlLogin, data).then(response => {
+    fetch(urlParticipants, data).then(response => {
         if (response.ok) {
             userName = loginInput.value;
             loginForm.classList.add("fade");
             setTimeout(() => loginForm.classList.add("hide"), 1000);
             setInterval(() => refreshStatus(obj), 5000);
             // REFACTOR
-            updateMessages();
+            setInterval(updateMessages, 3000);
+            setInterval(updateParticipants, 5000);
             chatroom.classList.remove("hide");
             chat.children[chat.children.length - 1].scrollIntoView();
         } else {
+            // <SMALL> NOME INVALIDO
             loginSubmit.classList.remove("hide");
             loginLoading.classList.add("hide");
         }
+    }).catch(error => {
+        loginSubmit.classList.remove("hide");
+        loginLoading.classList.add("hide");
     });
 }
 
@@ -58,19 +66,20 @@ function updateButton(e) {
 
 // CHAT ==========================================================================
 const chat = document.querySelector(".chat");
-const urlMessages = "https://mock-api.driven.com.br/api/v6/uol/messages";
-setInterval(updateMessages, 3000);
-updateMessages();
 
 function updateMessages() {
     fetch(urlMessages)
     .then((response) => response.json())
     .then((data) => {
         const fragment = document.createDocumentFragment();
-        data.forEach((e) => fragment.appendChild(createMessage(e)));
+        data.forEach(e => {
+            if (e.type === "private_message" && e.from !== userName && e.to !== userName && e.to !== "Todos") return;
+            fragment.appendChild(createMessage(e));
+        });
         chat.replaceChildren(fragment);
-        chat.children[chat.children.length - 1].scrollIntoView();
-    });
+        // REFACTOR
+        if (percent > 90 && time <= 0) chat.children[chat.children.length - 1].scrollIntoView();
+    }).catch(error => console.log(error)); // put a five turn count to refresh page
 }
 
 const chatbox = document.forms[1];
@@ -82,9 +91,9 @@ function submitMessage(e) {
 
     const obj = {
         from: userName,
-        to: "Todos",
+        to: userSelected,
         text: messagebox.value,
-        type: "message" // ou "private_message" para o bônus
+        type: visibility === "public" ? "message" : "private_message"
     };
 
     messagebox.value = "";
@@ -93,11 +102,61 @@ function submitMessage(e) {
     fetch(urlMessages, data).then(response => {
         if (response.ok) {
             updateMessages();
-        }
-    });
+            // chat.children[chat.children.length - 1].scrollIntoView();
+        } else window.location.reload();
+    }).catch(error => window.location.reload());
 }
 
-// DOM
+// SIDEBAR ==============================================================================================
+const sidebar = document.forms[2];
+const userList = sidebar.querySelector(".user-list");
+let userSelected = "Todos";
+let visibility = "public";
+
+// REFACTOR !!!!!!!!
+function updateParticipants() {
+    fetch(urlParticipants)
+    .then((response) => response.json())
+    .then((data) => {
+        const fragment = document.createDocumentFragment();
+        let found = false;
+        data.unshift({ name: "Todos" });
+        data.forEach(e => {
+            const user = createParticipant(e);
+            fragment.appendChild(user);
+            if (e.name === userSelected) { found = true; user.children[0].checked = true; };
+        });
+        userList.replaceChildren(fragment);
+        if (!found) { userSelected = "Todos"; userList.children[0].children[0].checked = true; };
+    }).catch(error => console.log(error));
+}
+
+function updateSelected() {
+    userSelected = this.htmlFor;
+}
+
+function updateVisibility(tag) {
+    visibility = tag.htmlFor;
+}
+
+const aside = document.querySelector("aside");
+const bg = document.querySelector(".bg");
+
+function showSidebar() {
+    aside.classList.remove("hide");
+    setTimeout(() => {
+        bg.classList.remove("fade");
+        sidebar.classList.remove("slide");
+    }, 1);
+}
+
+function hideSidebar() {
+    bg.classList.add("fade");
+    sidebar.classList.add("slide");
+    setTimeout(() => aside.classList.add("hide"), 500);
+}
+
+// DOM ==================================================================================================
 function createMessage(obj) {
     const message = document.createElement("div");
     message.classList.add(obj.type.replace("_", "-"));
@@ -137,6 +196,34 @@ function createMessage(obj) {
     return message;
 }
 
+function createParticipant(obj) {
+    const li = document.createElement("li");
+
+    const input = document.createElement("input");
+    input.type = "radio";
+    input.id = obj.name; // id
+    input.name = "participant";
+    input.setAttribute("data-identifier", "participant");
+
+    const label = document.createElement("label");
+    label.htmlFor = obj.name; // id
+
+    const icon = document.createElement("ion-icon");
+    icon.setAttribute("name", "person-circle");
+    if (obj.name === "Todos") icon.setAttribute("name", "people");
+
+    const span = document.createElement("span");
+    span.textContent = obj.name; // id
+
+    label.appendChild(icon);
+    label.appendChild(span);
+    label.addEventListener("click", updateSelected);
+    li.appendChild(input);
+    li.appendChild(label);
+
+    return li;
+}
+
 // HELPERS ==================================================================
 function createPostData(obj) {
     return {
@@ -144,4 +231,24 @@ function createPostData(obj) {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(obj),
     };
+}
+
+// STATISTICS
+let percent = 0;
+let time = 0;
+let interval;
+
+function scrolling() {
+    let height = chat.clientHeight;
+    let scrollHeight = chat.scrollHeight - height;
+    let scrollTop = chat.scrollTop;
+    percent = Math.floor(scrollTop / scrollHeight * 100);
+
+    // REFACTOR
+    time = 1000;
+    clearInterval(interval);
+    interval = setInterval(() => {
+        time -= 100;
+        if (time <= 0) clearInterval(interval);
+    }, 100);
 }
